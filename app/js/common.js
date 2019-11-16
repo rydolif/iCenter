0bbbff0970fa79b0e77df082ceae9b09305b6f01
Mykola Polishchuk <rudolifrudolif@gmail.com>
2019-11-16 16:03:48 +0200

    доробатки адаптив commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
 
+//----------------------------wowJS-------------------------------
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+  );
+  wow.init();
+  
 //----------------------------------------up----------------------------------
   $(window).scroll(function(){
       if($(this).scrollTop()>100){
@@ -14,20 +31,6 @@ $(function() {
       $('.up').addClass('up--active');
   }
 
-//-------------------------responsive---------------------------------------
-  var swiper = new Swiper('.raffle__wrap', {
-    slidesPerView: 2,
-    pagination: {
-      el: '.raffle__pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      767: {
-        slidesPerView: 1,
-        spaceBetween: 10
-      }
-    }
-  });
 
 //-------------------------slider---------------------------------------
   var swiper = new Swiper('.slider__container', {
